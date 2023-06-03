9f2fdbc5747c1f486922a9cce26467e3861108dd
action0709 <action0709@mail.ru>
2023-06-03 17:41:11 +0400

    ДЗ 9

---
+++ src/main/java/com/pro/sky/DZ9_Kurs2/DZ9_Kurs2Application.java
@@ -0,0 +1,11 @@
+package com.pro.sky.DZ9_Kurs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DZ9_Kurs2Application {
+    public static void main (String[] args){
+        SpringApplication.run(DZ9_Kurs2Application.class,args);
+    }
+}
