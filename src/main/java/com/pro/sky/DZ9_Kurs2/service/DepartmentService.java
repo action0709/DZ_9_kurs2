a258be8c5837dad4c851d20adcc76fb595a0c26a
action0709 <action0709@mail.ru>
2023-06-05 12:41:38 +0400

    ДЗ 9_2

--- src/main/java/com/pro/sky/DZ9_Kurs2/service/DepartmentService.java
+++ src/main/java/com/pro/sky/DZ9_Kurs2/service/DepartmentService.java
@@ -5,6 +5,9 @@ import com.pro.sky.DZ9_Kurs2.model.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService {
@@ -25,5 +28,17 @@ public class DepartmentService {
                 .filter(employee -> employee.getDepartment() == department)
                 .min(Comparator.comparingDouble(Employee::getSalary))
                 .orElseThrow(EmployeeNotFoundException::new);
+    }
+
+    public List<Employee> getAll (int department){
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == department)
+                .collect(Collectors.toList());
+
+    }
+
+    public Map<Integer,List<Employee>> getAll() {
+        return employeeService.getAll().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
     }
 }
--- src/main/java/com/pro/sky/DZ9_Kurs2/service/EmployeeService.java
+++ src/main/java/com/pro/sky/DZ9_Kurs2/service/EmployeeService.java
@@ -10,19 +10,28 @@ import java.util.*;
 
 @Service
 public class EmployeeService {
-    private final Map<String, Employee> employees = new HashMap();
+    private final Map<String, Employee> employees = new HashMap(MAX_SIZE);
     final static private int MAX_SIZE = 5;
+    public EmployeeService() {
+        Employee employee1 = new Employee("Petr", "Sidorov", 1, 10000);
+        Employee employee2 = new Employee("Олег", "Sidorov", 1, 30000);
+        Employee employee3 = new Employee("Мария", "Sidorovа", 1, 40000);
+        Employee employee4 = new Employee("Сергей", "Sidorov", 1, 50000);
+        employees.put(createKey(employee1), employee1);
+        employees.put(createKey(employee2), employee2);
+        employees.put(createKey(employee3), employee3);
+        employees.put(createKey(employee4), employee4);
+    }
 
-    public Employee add(String firstName, String lastName,int department,double salary) {
+    public Employee add(Employee employee) {
         if (employees.size() >= MAX_SIZE) {
             throw new EmployeeStorageIsFullException();
         }
-        Employee employeeToAdd = new Employee(firstName, lastName,department,salary);
-        if (employees.containsKey(createKey(firstName,lastName))) {
+        if (employees.containsKey(createKey(employee))) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.put(createKey(firstName,lastName),employeeToAdd);
-        return employeeToAdd;
+        employees.put(createKey(employee),employee);
+        return employee;
     }
 
 //    public Employee remove(String firstName, String lastName) {
